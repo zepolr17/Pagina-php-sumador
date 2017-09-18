6c5dd6135d34c340af380bdb6cb6cd8d3e7ec59c
Vivi <Vivi@Viviana>
2017-09-18 11:39:13 -0500

    clase sumar

---
+++ Sumar.php
@@ -0,0 +1,15 @@
+<?php
+class sumar{
+    function sumar($a,$b){
+      $resultado=$a+$b;
+      return $resultado;
+    }
+    
+    
+}
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
