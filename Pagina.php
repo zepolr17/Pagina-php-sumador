4e1a7169521752fdf66841e95e3607cc581f662d
Vivi <Vivi@Viviana>
2017-09-18 11:38:59 -0500

    Boton y cuadros de texto

---
+++ Pagina.php
@@ -0,0 +1,14 @@
+<html>
+    <body>
+        <form action="porcesador.php">
+            <p>a<p>
+            <input type="text" name="a">
+            <br>
+              <p>b<p>
+            <input type="text" name="b">
+            <br>
+            <input type="submit">
+        </form>
+    </body>
+</html>
+
